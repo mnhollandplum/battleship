fed4dcdd9c557d87d1ebc688e18c4ff24de35a96
Nikki Holland-Plum <mnhollandplum@gmail.com>
2018-09-02 14:05:53 -0600

    adds 2 initialized ships for computer

--- lib/computer.rb
+++ lib/computer.rb
@@ -1,2 +1,6 @@
 class Computer
+    attr_reader :ships
+  def initialize
+    @ships = 2
+  end
 end
--- test/computer_test.rb
+++ test/computer_test.rb
@@ -4,9 +4,16 @@ require './lib/computer'
 require 'minitest/autorun'
 require 'minitest/pride'
 
-class ComputerTest < Minitest::Test 
-    def test_computer_exists
-        computer = Computer.new
-        assert_instance_of Computer, computer 
-    end
+class ComputerTest < Minitest::Test
+  def test_computer_exists
+    computer = Computer.new
+    assert_instance_of Computer, computer
+  end
+
+  def test_computer_starts_with_two_ships
+    computer = Computer.new
+    assert_equal 2, computer.ships
+  end
+
+
 end
